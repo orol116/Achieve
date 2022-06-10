6d374435a0266f876438a0e9f23658fffeb56649
Rim <bono1223234@gmail.com>
2022-06-10 10:21:56 +0900

    작성한 게시글 조회 구현1회차

--- SemiProjectAchieve/src/main/java/edu/kh/Achieve/member/model/vo/CheckBoard.java
+++ SemiProjectAchieve/src/main/java/edu/kh/Achieve/member/model/vo/CheckBoard.java
@@ -1,5 +1,29 @@
 package edu.kh.Achieve.member.model.vo;
 
-public class CheckBoard {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-}
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckBoard {
+		
+		private int boardNo;
+		private String boardTitle;
+		private String boardContent;
+		private String createDate;
+		private String updateDate;
+		private int readCount;
+		private String boardState;
+		private int memberNo;
+		private String memberNickname;
+		private int boardCode;
+
+	}
+
+
