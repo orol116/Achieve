20d077b59188f461d757456db09c3d3e6eebe42a
Rim <bono1223234@gmail.com>
2022-06-13 09:52:48 +0900

    작성글 조회 구현 2회차 수정

--- SemiProjectAchieve/src/main/java/edu/kh/Achieve/member/model/dao/CheckBoardDAO.java
+++ SemiProjectAchieve/src/main/java/edu/kh/Achieve/member/model/dao/CheckBoardDAO.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-
+import edu.kh.Achieve.board.model.vo.Pagination;
 import edu.kh.Achieve.member.model.vo.CheckBoard;
 import edu.kh.Achieve.member.model.vo.CheckPagination;
 
@@ -36,66 +36,24 @@ public class CheckBoardDAO {
 			e.printStackTrace();
 		}
 	}
-
-	/** 게시판 전체 게시글 수 조회 DAO
+	/** 게시글 목록 조회 DAO
 	 * @param conn
-	 * @return listCount
+	 * @param type
+	 * @return listName
 	 * @throws Exception
 	 */
-	public int getListCount(Connection conn) throws Exception{
-
-		int listCount = 0;
+	public String selectListName(Connection conn, int type, int memNo) throws Exception{
+		String listName = null;
 		
 		try{
-			String sql = prop.getProperty("getCheckListCount");
-			stmt= conn.createStatement();	
-			rs=stmt.executeQuery(sql);
-			if(rs.next()) {
-				listCount = rs.getInt(1);
-			}
-		}finally{
-			close(rs);
-			close(stmt);
-		}
-		return listCount;
-	}
-
-	/** 게시판에서 일정한 범위의 목록 조회 DAO
-	 * @param conn
-	 * @param pagination
-	 * @param type
-	 * @return boardList
-	 * @throws Exception
-	 */
-	public List<CheckBoard> selectBoardList(Connection conn, CheckPagination pagination) throws Exception {
-		
-		List<CheckBoard> boardList = new ArrayList<CheckBoard>();
-		
-		try {
-			String sql = prop.getProperty("selectBoardList");
-			
-			// BETWEEN 구문에 들어갈 범위 계산
-			int start =  (pagination.getCurrentPage() - 1 ) * pagination.getLimit() + 1;
-			int end = start + pagination.getLimit()-1;
+			String sql = prop.getProperty("selectListName");
 			
 			pstmt = conn.prepareStatement(sql);
-			
-
-			pstmt.setInt(1, start);
-			pstmt.setInt(2, end);
-			
+			pstmt.setInt(1, memNo);
 			rs = pstmt.executeQuery();
 			
-			while(rs.next()) {
-				CheckBoard board = new CheckBoard();
-				
-//				board.setBoardNo(rs.getInt("BOARD_NO"));
-				board.setBoardTitle(rs.getString("BOARD_TITLE"));
-//				board.setMemberNickname(rs.getString("MEMBER_NICK"));
-				board.setCreateDate(rs.getString("CREATE_DT"));
-				board.setReadCount(rs.getInt("READ_COUNT"));
-
-				boardList.add(board);
+			if(rs.next()) {
+				listName = rs.getString(1);
 			}
 			
 		}finally {
@@ -103,6 +61,78 @@ public class CheckBoardDAO {
 			close(pstmt);
 		}
 		
+		return listName;
+	}
+	/** 작성글 수 조회 DAO
+	 * @param conn
+	 * @param memNo 
+	 * @return listCount
+	 * @throws Exception
+	 */
+	public int getListCount(Connection conn , int type, int memNo) throws Exception{
+		
+		int listCount = 0;
+		
+		try{
+	String sql = prop.getProperty("getListCount");
+			
+			pstmt= conn.prepareStatement(sql);
+			pstmt.setInt(1, memNo);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				listCount = rs.getInt(1);
+			}
+		}finally{
+			close(rs);
+			close(pstmt);
+		}
+		return listCount;
+		
+	}
+
+	/** 게시판에서 일정한 범위의 목록 조회 DAO
+	 * @param conn
+	 * @param pagination
+	 * @param type
+	 * @param memNo 
+	 * @return boardList
+	 * @throws Exception
+	 */
+	public List<CheckBoard> selectBoardList(Connection conn, CheckPagination pagination, int type, int memNo) throws Exception {
+		
+		List<CheckBoard> boardList = new ArrayList<CheckBoard>();
+		
+		try {
+			String sql = prop.getProperty("selectBoardList");
+			
+			// BETWEEN 구문에 들어갈 범위 계산
+			int start =  (pagination.getCurrentPage() - 1) * pagination.getLimit() +1;
+			int end = start + pagination.getLimit()-1;
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, memNo);
+			pstmt.setInt(2, start);
+			pstmt.setInt(3, end);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				CheckBoard board = new CheckBoard();
+				
+				board.setBoardNo(rs.getInt("BOARD_NO"));
+				board.setBoardTitle(rs.getString("BOARD_TITLE"));
+				board.setCreateDate(rs.getString("CREATE_DT"));
+				board.setReadCount(rs.getInt("READ_COUNT"));
+
+				boardList.add(board);
+			
+			}
+			
+		}finally {
+			close(rs);
+			close(pstmt);
+		}
 		
 		return boardList;
 	}
@@ -145,19 +175,17 @@ public class CheckBoardDAO {
 		
 		try {
 			String sql = prop.getProperty("deleteBoard");
-			
 			pstmt = conn.prepareStatement(sql);
-			
 			pstmt.setInt(1, boardNo);
 			result = pstmt.executeUpdate();
-			
 			
 		}finally {
 			close(pstmt);
 		}
 		
-		
 		return result;
 	}
+
+
 
 }
